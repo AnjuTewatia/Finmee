1ad54adb864b1f828ac1bc5d1c08ef6a2b38a272
AnjuTewatia <anjutewatia008@gmail.com>
2023-10-05 20:14:08 +0530

    homepagentwist

---
+++ ntwist/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import "./Navbar.css";
+import React from "react";
+import { useState, useEffect } from "react";
+function NavLink() {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    // Function for scrolling navbar
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        // If the user has scrolled down, add the "navbar-white" class
+        setScrolling(true);
+      } else {
+        // If the user is at the top of the page, remove the "navbar-white" class
+        setScrolling(false);
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`navbar ${scrolling ? "navbar-white" : ""}`}>
+      <div>
+        <div id="navbar">
+          {/* Logo */}
+          <div>
+            <img
+              src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png"
+              alt=""
+            />
+          </div>
+          {/*  Second half  */}
+          <div id="second">
+            <h4>Home</h4>
+            <h4> Industries</h4>
+            <h4>Al Software</h4>
+            <h4>Blog</h4>
+            <h4>Contact Us</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavLink;
---
+++ ntwist/src/Components/Pages/Home.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Box,
+  Image,
+  SimpleGrid,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import { IoMdArrowDropdown } from "react-icons/io";
+const Home = () => {
+  return (
+    <div>
+      <Box>
+        {/*  background image */}
+        <Box
+          height="670px"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+          backgroundImage="url(https://www.miningmonthly.com/w-images/d734243d-c0c2-45ce-a6a6-9d3ecba6ed32/2/Dassault-1680x600.png)"
+        >
+          {/*  content written over background image */}
+          <Box paddingTop="100px">
+            <SimpleGrid columns={[1, 1, 1, 2]} gap={"30px"}>
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Box textAlign="left">
+                  <Heading color="white">
+                    Data-Powered Solutions <br />
+                    for Industrial Excellence
+                  </Heading>
+
+                  <Button color="white" backgroundColor="blue" padding="20px">
+                    Read More
+                  </Button>
+                </Box>
+              </Box>
+              <Box>
+                <Image src="https://ntwist.com/wp-content/uploads/2023/01/home-image-icons-v6.png"></Image>
+              </Box>
+            </SimpleGrid>
+          </Box>
+          <Box marginLeft={"30px"} position="fixed">
+            <Button padding={"30px 10px"}>
+              <Image
+                width={"60%"}
+                height={"30px"}
+                src="https://i.imgur.com/J6vHKA4.png"
+              ></Image>
+              <IoMdArrowDropdown fontSize={"20px"} />
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
